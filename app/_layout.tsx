2293a17ff48c5248881574080775ffbf1bb863aa
preethib20 <145477588+preethib20@users.noreply.github.com>
2025-07-15 15:15:47 -0400

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,8 +9,18 @@ import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+
+
 
 export default function RootLayout() {
+   return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack screenOptions={{ headerShown: false }} />
+    </GestureHandlerRootView>
+  );
+}
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
